008f4cdbca84ec18c050bf84e9c768cfeaac1a23
Takshak Ramteke <takshakramteke0708@gmail.com>
2021-11-24 17:10:19 +0000

    Feat : Completed the UI of Contact Page Form need's to be added

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,8 @@
 import Head from 'next/head'
 import Layout from '../components/layout';
+import commonStyle from '../styles/Common.module.css';
+import styles from '../styles/Contact.module.css';
+import { LinkedinOutlined, InstagramOutlined, FacebookOutlined, TwitterOutlined} from '@ant-design/icons'
 
 const Contact = () =>{
   return(
@@ -9,7 +12,25 @@ const Contact = () =>{
           Contact Page
         </title>
       </Head>
-      <h1>Contact Page</h1>
+      <div className={commonStyle.verticalContainer}>
+        <h2 className={styles.H2}>Wanna Get in Touch !</h2>
+        <div className={styles.socials}>
+          <h3 className={styles.H3}>Follow us on</h3>
+          <LinkedinOutlined className={styles.icons}/>
+          <InstagramOutlined className={styles.icons}/>
+          <FacebookOutlined className={styles.icons}/>
+          <TwitterOutlined className={styles.icons}/>
+        </div>
+        <div className={styles.contactForm}>
+          <h3 className={styles.H3}>Drop A Hello !</h3>
+          <form>
+            <h3 className={styles.H3}>First name:</h3>
+            <input type="text" name="fname" /><br />
+            <h3 className={styles.H3}>Email:</h3>
+            <input type="email" name="lname" />
+          </form>
+        </div>
+      </div>
     </Layout>
   );
 }
