6cb2cd5ff6e047e327225695b1e0d3985abfbe6b
Takshak Ramteke <takshakramteke0708@gmail.com>
2021-11-24 15:51:19 +0000

    Feat : Events Page Complete.

--- pages/events.js
+++ pages/events.js
@@ -1,5 +1,7 @@
-import Head from 'next/head'
+import Head from 'next/head';
 import Layout from '../components/layout';
+import commonStyle from '../styles/Common.module.css';
+import styles from '../styles/Events.module.css';
 
 const Events = () =>{
   return(
@@ -9,7 +11,40 @@ const Events = () =>{
           Events Page
         </title>
       </Head>
-      <h1>Events Page</h1>
+      <div className={commonStyle.centerContainer}>
+        <div className={styles.eventContainer}>
+          <div className={styles.event}>
+            Event 1
+          </div>
+          <div className={styles.event}>
+            Event 2
+          </div>
+          <div className={styles.event}>
+            Event 3
+          </div>
+          <div className={styles.event}>
+            Event 4
+          </div>
+          <div className={styles.event}>
+            Event 5
+          </div>
+          <div className={styles.event}>
+            Event 6
+          </div>
+          <div className={styles.event}>
+            Event 7
+          </div>
+          <div className={styles.event}>
+            Event 8
+          </div>
+          <div className={styles.event}>
+            Event 9
+          </div>
+          <div className={styles.event}>
+            Event 10
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
