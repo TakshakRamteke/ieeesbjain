b8125a5f432da583e279d88144e61702a63edb49
TakshakRamteke <takshakramteke0708@gmail.com>
2021-11-26 19:07:24 +0530

    Fix : Fixing the Background Image.

--- components/navsection.js
+++ components/navsection.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import styles from './navsection.module.css';
 import Sidebar from './sidebar';
 
@@ -5,6 +6,11 @@ export default function NavSection({children}){
     return (
         <div className={styles.container}>
             <div className={styles.contentArea}>
+                <Image
+                src="/background.png"
+                layout="fill"
+                className = {styles.backgroundImage}
+                />
                 <main>
                     {children}
                 </main>
